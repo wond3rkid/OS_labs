e70d25ba951326361f6726ee9640c56e08fe9c2c
savilovaae <a.savilova@g.nsu.ru>
2024-03-30 03:15:04 +0700

    some upd. now realising file reading

--- files_and_file_systems/reverse_copy/main.c
+++ files_and_file_systems/reverse_copy/main.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 #define EXIT_SUCCESS 0
 #define EXIT_ERROR 1
@@ -51,33 +52,36 @@ void create_directory(char *name) {
     mkdir(name, 0700);
 }
 
+void create_file(char *name) {
+    FILE *file_ptr = fopen(name, "w");
+    fclose(file_ptr);
+}
+
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    /*if (argc != 2) {
         fprintf(stderr, "You have to pass the directory path");
         return EXIT_ERROR;
     }
-    
+    char *full_catalog_path = argv[1];
+    */
     char *full_catalog_path = "/home/freak1rget/CLionProjects/oslab3/cmake-build-debug/tmp";
     int name_length = 0, path_length = 0;
     get_lengths(&name_length, &path_length, full_catalog_path);
     char *catalog_name = malloc(sizeof(char) * name_length);
-
     char *path_to_catalog = malloc(sizeof(char) * path_length);
     split_catalog_name_from_path(full_catalog_path, catalog_name, path_to_catalog, path_length, name_length);
     char new_catalog_name;
-
     reverse_string(catalog_name, &new_catalog_name);
-
     char new_path[name_length + path_length + 1];
-
-    get_full_path(&new_catalog_name, path_to_catalog, &new_path);
-
+    get_full_path(&new_catalog_name, path_to_catalog, (char *) &new_path);
     new_path[path_length + name_length + 1] = '\0';
 
-    fprintf(stderr, "3. %s \n", new_path);
+    //create_directory(new_path);
+    //create_file("/home/freak1rget/CLionProjects/oslab3/cmake-build-debug/pmt/my.txt");
 
-    create_directory(new_path);
     free(catalog_name);
     free(path_to_catalog);
     return EXIT_SUCCESS;
+
 }
